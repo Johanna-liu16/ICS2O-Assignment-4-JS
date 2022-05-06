1b106d41f57763f53e87670e1b7cf2be6a752352
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-06 08:13:29 -0400

    add

--- js/script.js
+++ js/script.js
@@ -17,46 +17,45 @@
 /**
  * Input
  */
-function myButtonClicked() {
-  const largePizza = document.getElementById("largePizza").checked;
-  const xLargePizza = document.getElementById("xLargePizza".checked);
-  const t1 = document.getElementById("t1").checked);
-  const t2 = document.getElementById("t2").checked;
-  const t3 = document.getElementById("t3").checked;
-  const t4 = document.getElementById("t4").checked;
-    
-  var sizePrice = 0
-  var toppingPrice = 0
-  var tax
-  var price
+function getPizza() {
+  var price = 0
+  var top = 0
+  var total = 0
+  var tax = 0
 
-    if (largePizza.checked == true){
-        sizePrice = 6.0
-        document.getElementById("largePizza").innerHTML = "Large"
-    } else {
-        sizePrice = 10.0
-        document.getElementById("largePizza").innerHTML = "Extra large"
-    }
+  var s1 = document.getElementById("s1")
+  var s2 = document.getElementById("s2")
 
-    if (t1 == true) {
-        toppingPrice = 1.0;
-        document.getElementById("t1").innerHTML = "Topping 1"
-    } else if (t2 == true) {
-        toppingPrice = 1.75;
-        document.getElementById("t2").innerHTML = "Topping 2"
-    } else if (t3 == true) {
-        toppingPrice = 2.5;
-        document.getElementById("t3").innerHTML = "Topping 3"
-    } else {
-        toppingPrice = 3.35;
-        document.getElementById("t4").innerHTML = "Topping 4"
-    }
-
-    //output
-    tax = (sizePrice + toppingPrice) * 0.13;
-    price = tax + (sizePrice + toppingPrice);
-    
-    document.getElementById("answers").innerHTML =
-      "Your pizza will be $" + price.toFixed(2) + ".";
+  if (s1.checked == true) {
+    price = 6.0
+  } else if (s2.checked == true) {
+    price = 10.0
   }
+
+  var t1 = document.getElementById("topping1")
+  var t2 = document.getElementById("topping2")
+  var t3 = document.getElementById("topping3")
+  var t4 = document.getElementById("topping4")
+
+  if (t1.checked == true) {
+    top = 1.0
+  }
+
+  if (t2.checked == true) {
+    top = 1.75
+  }
+
+  if (t3.checked == true) {
+    top = 2.5
+  }
+
+  if (t4.checked == true) {
+    top = 3.35
+  }
+
+  tax = (price + top) * 0.13
+  total = tax + (price + top)
+
+  document.getElementById("total_result").innerHTML =
+    "Your Current Total is $ " + total.toFixed(2)
 }
